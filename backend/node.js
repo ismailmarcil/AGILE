cc746e5d634f407b5e7e4567cc52324402af5385
otkito <tkitomar@gmail.com>
2025-11-21 10:02:19 +0100

    Implement Node class for map intersections

---
+++ backend/node.js
@@ -0,0 +1,59 @@
+/**
+ * Class representing a map node (intersection)
+ */
+class Node {
+    /**
+     * Unique identifier of the node
+     * @type {string|number}
+     */
+    id;
+
+    /**
+     * Latitude coordinate of the node
+     * @type {number}
+     */
+    latitude;
+
+    /**
+     * Longitude coordinate of the node
+     * @type {number}
+     */
+    longitude;
+
+    /**
+     * Constructor for the Node class
+     * @param {string|number} id - Unique identifier of the node
+     * @param {number} latitude - Latitude value
+     * @param {number} longitude - Longitude value
+     */
+    constructor(id, latitude, longitude) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    /**
+     * Returns a JSON representation of the node
+     * @returns {Object}
+     */
+    toJSON() {
+        return {
+            id: this.id,
+            latitude: this.latitude,
+            longitude: this.longitude
+        };
+    }
+
+    /**
+     * Returns a textual summary of the node
+     * @returns {string}
+     */
+    toString() {
+        return `Node ${this.id} (${this.latitude}, ${this.longitude})`;
+    }
+}
+
+// Export for Node.js
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Node;
+}
