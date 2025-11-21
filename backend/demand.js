55f7233a28382d860e3eee278e5e985744d2f665
otkito <tkitomar@gmail.com>
2025-11-21 10:09:03 +0100

    Implement Demand class for Pickup & Delivery requests

---
+++ backend/demand.js
@@ -0,0 +1,68 @@
+/**
+ * Class representing a Pickup & Delivery request
+ */
+class Demand {
+    /**
+     * Pickup address (node ID)
+     * @type {string|number}
+     */
+    pickupAddress;
+
+    /**
+     * Delivery address (node ID)
+     * @type {string|number}
+     */
+    deliveryAddress;
+
+    /**
+     * Pickup duration in seconds
+     * @type {number}
+     */
+    pickupDuration;
+
+    /**
+     * Delivery duration in seconds
+     * @type {number}
+     */
+    deliveryDuration;
+
+    /**
+     * Constructor for the Demand class
+     * @param {string|number} pickupAddress - Node ID for pickup
+     * @param {string|number} deliveryAddress - Node ID for delivery
+     * @param {number} pickupDuration - Duration at pickup (seconds)
+     * @param {number} deliveryDuration - Duration at delivery (seconds)
+     */
+    constructor(pickupAddress, deliveryAddress, pickupDuration, deliveryDuration) {
+        this.pickupAddress = pickupAddress;
+        this.deliveryAddress = deliveryAddress;
+        this.pickupDuration = pickupDuration;
+        this.deliveryDuration = deliveryDuration;
+    }
+
+    /**
+     * Returns a JSON representation of the demand
+     * @returns {Object}
+     */
+    toJSON() {
+        return {
+            pickupAddress: this.pickupAddress,
+            deliveryAddress: this.deliveryAddress,
+            pickupDuration: this.pickupDuration,
+            deliveryDuration: this.deliveryDuration
+        };
+    }
+
+    /**
+     * Returns a textual summary of the demand
+     * @returns {string}
+     */
+    toString() {
+        return `Demand - Pickup at ${this.pickupAddress} (${this.pickupDuration}s), Delivery at ${this.deliveryAddress} (${this.deliveryDuration}s)`;
+    }
+}
+
+// Export for Node.js
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Demand;
+}
