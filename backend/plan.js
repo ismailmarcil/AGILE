4afa567edd8423c8057da3b036171a64746f9895
otkito <tkitomar@gmail.com>
2025-11-21 09:43:42 +0100

    Add Plan class with constructor and properties

--- backend/plan.js
+++ backend/plan.js
@@ -1,1 +1,12 @@
+export default class Plan {
 
+    noeuds = [];
+    troncons = [];
+    entrepot = null;
+
+    constructor(noeuds = [], troncons = [], entrepot = null) {
+        this.noeuds = noeuds;
+        this.troncons = troncons;
+        this.entrepot = entrepot;
+    }
+}
