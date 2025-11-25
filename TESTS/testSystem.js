470e2bbe3040d1b382c4c7660d456c324e873819
ismailmarcil <marcilismaill@gmail.com>
2025-11-25 11:44:05 +0100

    test de l'affichage du json (V0 loadDemands)

---
+++ TESTS/testSystem.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const xml2js = require("xml2js");
+const System = require("../backend/system.js");
+
+(async () => {
+    const system = new System([]);
+    await system.loadDemandsFromXML("../demandeGrand9.xml");
+})();
