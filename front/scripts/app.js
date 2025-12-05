06be63606b42b3a49b2a89a4d6517b44c980d2e7
unknown <tkitomar@gmail.com>
2025-12-05 11:33:38 +0100

    Corrections

--- front/scripts/app.js
+++ front/scripts/app.js
@@ -192,7 +192,7 @@ async function handleLoadHistory() {
             view.displayTour(tour);
         }
 
-        updateTimelineFromTour(tour);
+        updateTimelineFromTour(tour, true);
         updateCourierInfo(tour);
     };
 
